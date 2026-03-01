86716ca2eec2dd8dac48892d9794a2fc81608ce9
mohamed eid <meid368@gmail.com>
2026-03-01 17:01:54 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+Route::group(
+[
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+],
+function()
+{
+    Route::get('/', function () {
+        return view('dashboard');
+    });
+
+});
+
